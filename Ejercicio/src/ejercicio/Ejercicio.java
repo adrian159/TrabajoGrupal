ca6dc51cf010ed64ce74903f7111da37256e5599
VALENTINA <VALENTINA@Rnavy>
2016-02-01 23:52:16 -0500

    valen

--- Ejercicio/src/ejercicio/Ejercicio.java
+++ Ejercicio/src/ejercicio/Ejercicio.java
@@ -15,8 +15,8 @@ public class Ejercicio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("SEBASTIAN PARRA");
-        
+        System.out.println("SEBASTIAN PARRA");
+
         Motorola Objeto6 = new Motorola();
         Objeto6.setColor("Negro");
         Objeto6.setSerial("1548654852");
@@ -24,10 +24,18 @@ public class Ejercicio {
         System.out.println(Objeto6.getSerial());
         System.out.println("SEBASTIAN H.");
         Nokia objeto2 = new Nokia();
-        objeto2.setModelo("nokia 1100"); 
+        objeto2.setModelo("nokia 1100");
         System.out.println(objeto2.getModelo());
         objeto2.setColor("Verde");
         System.out.println(objeto2.getColor());
+
+        System.out.println("Valentina");
+        Huawei objeto4 = new Huawei();
+        objeto4.setColor("blanco");
+        objeto4.setSerial("4852");
+        System.out.println(objeto4.getColor());
+        System.out.println(objeto4.getSerial());
+
     }
-    
+
 }
---
+++ Ejercicio/src/ejercicio/Huawei.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicio;
+
+/**
+ *
+ * @author VALENTINA
+ */
+public class Huawei {
+    String color;
+    String serial;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+}
